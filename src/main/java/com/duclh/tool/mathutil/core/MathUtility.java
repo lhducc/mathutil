1180976be1434ae44cad4ec96036f1d970b182f0
lhducc <pduc2200@gmail.com>
2025-06-12 17:11:09 +0700

    update main code to use recursion and to see green 4ever

--- src/main/java/com/duclh/tool/mathutil/core/MathUtility.java
+++ src/main/java/com/duclh/tool/mathutil/core/MathUtility.java
@@ -10,22 +10,41 @@ public class MathUtility {
     //KO CÓ GIAI THỪA SỐ ÂM
     //0! = 1
     //20! VỪA ĐỦ KIỂU LONG, TỨC LÀ 21! VƯỢT KIỂU LONG
+
     public static long getFactorial(int n) {
         //đem code chính ra sửa gọi là REFACTOR/REFACTORING
         if(n < 0 || n > 20)
             throw new IllegalArgumentException("n must be between 0 and 20");
-         //bạn đưa invalid data, tớ ném ra ngoại lệ, hàm ko đc chạy thành công, ko value trả về
+        //bạn đưa invalid data, tớ ném ra ngoại lệ, hàm ko đc chạy thành công, ko value trả về
 
-        if(n == 0)
+        if(n == 0 || n == 1)
             return 1;
 
-        //sống sót tới đây n = 1..19
-        //for mà tính
-        long result = 1; //thuật toán dồn con heo đất
-        for(int i = 1; i <= n; i++)
-            result *= i;
-
-        return result;
+        //ĐỆ QUY - RECURSION - GỌI LẠI CHÍNH MÌNH VỚI QUY MÔ NHỎ HƠN
+        return n * getFactorial(n - 1);
     }
+    //ĐEM LÊN SERVER MÀ RA MÀU XANH
+    //1. CODE TỐI ƯU NGON VÌ PASS TEST CASE
+    //2. TEST LẠI THỨ TỪNG TEST RẤT MẤT CÔNG, NAY CÓ SCRIPT TEST TỰ ĐỘNG, KHỎE RE
+    //    REGRESSION TESTING, KIỂM THỬ HỒI QUY
+
+
+//    public static long getFactorial(int n) {
+//        //đem code chính ra sửa gọi là REFACTOR/REFACTORING
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException("n must be between 0 and 20");
+//         //bạn đưa invalid data, tớ ném ra ngoại lệ, hàm ko đc chạy thành công, ko value trả về
+//
+//        if(n == 0)
+//            return 1;
+//
+//        //sống sót tới đây n = 1..19
+//        //for mà tính
+//        long result = 1; //thuật toán dồn con heo đất
+//        for(int i = 1; i <= n; i++)
+//            result *= i;
+//
+//        return result;
+//    }
 
 }
